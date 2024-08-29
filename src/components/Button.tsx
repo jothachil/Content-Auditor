a088f624e72b14d00ec900b495aa7845caedb378
John Thachil <11116943+jothachil@users.noreply.github.com>
2024-08-29 22:25:09 +0530

    Added Components

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -6,7 +6,14 @@ interface ButtonProps {
 }
 
 const Button: React.FC<ButtonProps> = ({ label, onClick }) => {
-  return <button onClick={onClick}>{label}</button>;
+  return (
+    <button
+      className=" bg-orange-500 text-white text-xss p-2 rounded w-full hover:bg-orange-700 transition-all font-medium"
+      onClick={onClick}
+    >
+      {label}
+    </button>
+  );
 };
 
 export default Button;
--- src/ui.tsx
+++ src/ui.tsx
@@ -3,10 +3,17 @@ import * as ReactDOM from "react-dom";
 import "./ui.css";
 import { pluginApi } from "./api";
 import Button from "./components/Button";
+import Dropdown from "./components/Dropdown";
+import InputField from "./components/InputField";
 declare function require(path: string): any;
 
 function App() {
   const inputRef = React.useRef<HTMLInputElement>(null);
+  const options = [
+    { value: "Option 1", label: "Option 1" },
+    { value: "Option 2", label: "Option 2" },
+    { value: "Option 3", label: "Option 3" },
+  ]; // Updated options format
 
   const onCreate = () => {
     const count = Number(inputRef.current?.value || 0);
@@ -18,10 +25,34 @@ function App() {
     pluginApi.exit();
   };
 
+  const onSelect = (option: { value: string; label: string }) => {
+    console.log(`Selected: ${option.label}`);
+  };
+
   return (
-    <main className="bg-white h-[100vh] flex flex-col justify-center items-center w-full">
-      <Button label="Create" onClick={onCreate} />
-      <Button label="Cancel" onClick={onCancel} />
+    <main className="bg-white h-[100vh] relative ">
+      <div className="pt-2 flex items-center gap-x-2 p-2">
+        <div className="flex-1">
+          <Dropdown
+            options={options}
+            onSelect={(value) =>
+              onSelect(options.find((option) => option.value === value)!)
+            }
+          />
+        </div>
+      </div>
+      <hr className="border-t  border-neutral-200 " />
+      <div className="pt-2 flex items-center gap-x-2 p-2">
+        <div className="flex-1">
+          <InputField />
+        </div>
+      </div>
+      <div className=" absolute bottom-0 w-full ">
+        <hr className="border-t border-neutral-200 " />
+        <div className="p-2">
+          <Button label="Create" onClick={onCreate} />
+        </div>
+      </div>
     </main>
   );
 }
